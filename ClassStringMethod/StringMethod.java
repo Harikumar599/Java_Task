457161a98d0d63a0ee6175db8f2ea70bd6be90b2
Hari kumar <126227017+Harikumar599@users.noreply.github.com>
2023-07-20 18:21:09 +0530

    String method

---
+++ ClassStringMethod/StringMethod.java
@@ -0,0 +1,59 @@
+class StringMethod{
+
+public static void main(String args[])
+    {
+   
+      String name = "Batch-17 is toxic";
+
+      //To check whether the string object contains given value or not.
+      System.out.println("contains():" + name.contains("toxic"));
+   
+      //To check whether the string object contains given value or not.
+      System.out.println("equals():" + name.equals("Batch-17 is toxic"));
+
+       //To check whether the string object contains given value or not.
+      System.out.println("equalsIgnoreCase():" + name.equalsIgnoreCase("Batch-17 is Toxic"));
+    
+       //To check whether the string object contains given value or not.
+      System.out.println("concat():" + name.concat(",but something \"Sweet!\""));
+        
+       //To check whether the string object equals given value or not. 
+	System.out.println("compareTo(): " + str1.compareTo()); 
+	
+	//To check whether the string object equals given value or not. 
+	System.out.println("compareToIgnoreCase(): " + str1.compareToIgnoreCase()); 
+
+	//To check whether the string object equals given value or not. 
+	System.out.println("indexOf(): " + str1.indexOf()); 
+
+	//To check whether the string object equals given value or not. 
+	System.out.println("isEmpty(): " + str1.isEmpty()); 
+
+	//To check whether the string object equals given value or not. 
+	System.out.println("replace(): " + str1.replace()); 
+
+	//To check whether the string object equals given value or not. 
+	System.out.println("substring(): " + str1.substring()); 
+
+	//To check whether the string object equals given value or not. 
+	System.out.println("trim(): " + str1.trim(9)); 
+
+	//To check whether the string object equals given value or not. 
+	System.out.println("hashCode(): " + str1.hashCode()); 
+
+	//To check whether the string object equals given value or not. 
+	System.out.println("format(): " + str1.format()); 
+
+	//To check whether the string object equals given value or not. 
+	System.out.println("join(): " + str1.join()); 
+
+	//To check whether the string object equals given value or not. 
+	System.out.println("replaceAll(): " + str1.replaceAll()); 
+
+	//To check whether the string object equals given value or not. 
+	System.out.println("split(): " + str1.split());
+
+     }
+
+
+                   }
