a6ed187122b0d9cb4365f13107d450f0439232d1
Hari kumar <126227017+Harikumar599@users.noreply.github.com>
2023-07-21 10:05:19 +0530

    Logical Ternary Shift Task

---
+++ A_Moodle_Task/Coding Practice 1.6.1/kgm.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class kgm{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = 10,b = 2;
+        System.out.println(a>b);
+        System.out.println(a>=b);
+        System.out.println(a<b);
+        System.out.println(a<=b);
+        System.out.println(a==b);
+        System.out.println(a!=b); 
+}
+}
---
+++ A_Moodle_Task/Coding Practice 1.6.4/kgm.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class kgm{
+    public static void main(String args[]){
+        int a = 10,b = 2;
+      int min=(a>b)?a:b;
+      System.out.println(min);
+    }
+    
+}
